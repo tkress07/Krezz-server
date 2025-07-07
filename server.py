8f7f1fd769e135170054d56f3b7ef8d36976aee5
tkress07 <tanner.j.kress@gmail.com>
2025-07-07 08:49:13 -0400

    Update server.py

--- server.py
+++ server.py
@@ -56,4 +56,6 @@ def stripe_webhook():
     return jsonify(success=True)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
